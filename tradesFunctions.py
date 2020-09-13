64ac2c3d3c93ab27e185378f7a7e7a3bf2084a8e
flame-alchemist <sharathmenon10@gmail.com>
2020-09-13 13:42:15 +0530

    Helper functions for trade apis

---
+++ tradesFunctions.py
@@ -0,0 +1,29 @@
+#function to create a new/update trade data
+def newTrade(trade, security, request, mongo):
+    tradeType = request.json["tradeType"]
+    price = request.json["price"]
+    shares = request.json["shares"]
+    new_data = {}
+    secID = ""
+
+    if trade==None:
+        secID = request.json["secID"]    
+    elif security==None:
+        secID = trade["secID"]
+        security = mongo.db.portfolio.find_one({"_id":secID})
+
+    if tradeType=="Sell":
+        totalShares = security["totalShares"]
+        if shares >= totalShares:
+            return -1
+        else:
+            shares = -shares
+        new_data = {"secID":secID, "tradeType":tradeType, "shares":shares, "price":0}
+
+    elif tradeType=="Buy":
+        new_data = {"secID":secID, "tradeType":tradeType, "shares":shares, "price":price}
+    
+    else:
+        return -2
+    
+    return new_data
